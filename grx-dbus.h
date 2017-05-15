48e78a1eaecd50729b95131df5bc4090d107904d
Alberto Avidad Fernandez <alberto@avidad.com>
2017-05-15 18:23:40 +0200

    Creación de los metodos de la clase grx_arp

--- grx-dbus.h
+++ grx-dbus.h
@@ -2,24 +2,32 @@
 #define GRX_DBUS
 #define SERVICE_NAME    "grx.dbus.service"
 #define BASE_DATOS "/var/lib/grx/conexiones.db"
-#define ARP_SCAN "/usr/sbin/arp-scan -q -x -g "
+#define ARP_SCAN "/usr/sbin/arp-scan -q -x -g -G 100 "
 #include <QtSql/QSqlDatabase>
 #include <QtSql/QSqlQuery>
 #include <QtSql/QSqlError>
 #include <QtCore/QObject>
 #include "QtDebug"
 #include <QDBusVariant>
-#include <QProcess>
+#include <QSqlRecord>
 
 class GrxArp: public QObject
 {
     Q_OBJECT
 public slots:
-    Q_SCRIPTABLE QString busca_equipos(const QString &arg);
-    Q_SCRIPTABLE QString busca_equipo_terminados_en(const QString i, const QString &arg);
-    Q_SCRIPTABLE QList<QVariant> ips_nodos();
+    Q_SCRIPTABLE QString busca_router(const QString &arg);
+    Q_SCRIPTABLE QString busca_router_muestra_nombre(const QString &nombres);
+    Q_SCRIPTABLE QList<QVariant> ip_nodos();
+    Q_SCRIPTABLE QString ip_nodos_string();
     Q_SCRIPTABLE QList<QVariant> consulta_sql(const QString &arg,int i);
-    Q_SCRIPTABLE QString busca_ips_nodos();
+    Q_SCRIPTABLE QList<QVariant> consulta_sql_todo(const QString &arg);
+    Q_SCRIPTABLE QString busca_todos_routers();
+    Q_SCRIPTABLE QString consulta_nombre(const QString &ip);
+    Q_SCRIPTABLE QString consulta_ip (const QString &nombre);
+    Q_SCRIPTABLE QString busca_nodo_por_ip(const QString &nodo);
+    Q_SCRIPTABLE QString busca_nodo_por_nombre(const QString &nodo);
+    Q_SCRIPTABLE bool esta_nodo_por_nombre(const QString &nodo);
+    Q_SCRIPTABLE bool esta_nodo_por_ip(const QString &nodo);
 private:
     QSqlDatabase db;
     QSqlQuery consultar;
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "grx-dbus.h"
 #include <QList>
 
-QString buscaEquipos(const QString &arg)
+QString arp_scan(const QString &arg)
 {
        QProcess process;
        process.start(arg);
@@ -30,70 +30,173 @@ QList<QVariant> ipsNodos()
        return vector;
 }
 
-QString GrxArp::busca_equipos(const QString &arg)
+QString ipsNodosString()
 {
-       return buscaEquipos( ARP_SCAN +arg);
+       QString vector;
+       QSqlQuery consultar;
+       if (consultar.exec("select ip from NODOS")){
+           while (consultar.next()){
+               vector.append(consultar.value(0).toString()+" ");
+           }
+       }
+       else{
+           qDebug()<<"Error No se ha podido realizar la consulta "<< consultar.lastError();
+       }
+       return vector;
 }
 
-QString GrxArp::busca_equipo_terminados_en(const QString i,const QString &arg )
+QString GrxArp::busca_router(const QString &arg)
 {
-
-    return buscaEquipos( ARP_SCAN + arg + " -G " + i);
+       return arp_scan( ARP_SCAN + arg);
 }
 
-/*QList<QVariant> GrxArp::ips_nodos()
+QString GrxArp::busca_router_muestra_nombre(const QString &nombres)
 {
-        QString consulta;
-        QList<QVariant> vector;
-        consulta.append("select ip from NODOS");
-        QSqlQuery consultar;
-        if (consultar.exec(consulta)){
-            while (consultar.next()){
-                vector.append(consultar.value(0).toString());
-            }
-        }
-        else{
-            qDebug()<<"Error No se ha podido realizar la consulta "<< consultar.lastError();
-        }
-return vector;
-}
-*/
+       QString arp;
+       arp = arp_scan(ARP_SCAN + nombres);
+       qDebug() << arp;
+       QStringList nodoList = arp.split('\n');
+       qDebug() << nodoList.;
+       QStringList nodoList2 = arp.split('\n').first().split('\t');
+       qDebug() << nodoList2;
 
-QList<QVariant> GrxArp::ips_nodos(){
+       //QStringList nodoList = arp.split('\n').first().split('\t'); //arp devuelve algo parecido a esto "192.168.1.1\tdd:dd:dd:dd:dd:dd"
+       //if (!nodoList.empty()){
+        //   while (nodoList.)
+       //}
+       //return nodoList.first();//con esto cogemos solo la ip
+       //return arp_scan( ARP_SCAN + arg);
+       return "ok";
+}
+
+QList<QVariant> GrxArp::ip_nodos(){
 
        return ipsNodos();
 }
 
+QString GrxArp::ip_nodos_string(){
 
+    return ipsNodosString();
 
-QString GrxArp::busca_ips_nodos()
-{
-        QString consulta;
-        QProcess process;
-        QString vector;
-        consulta.append("select ip from NODOS");
-        QSqlQuery consultar;
-        if (consultar.exec(consulta)){
-            while (consultar.next()){
-                   vector.append(consultar.value(0).toString()+" ");
-            }
-         qDebug() << vector;
-         process.start("/usr/sbin/arp-scan -q -x -g "+vector);
-         process.waitForFinished(-1);
-        }
-        else{
-            qDebug()<<"Error No se ha podido realizar la consulta "<< consultar.lastError();
-        }
-return process.readAllStandardOutput();
 }
 
-QList<QVariant> GrxArp::consulta_sql(const QString &arg,int i)
+QString GrxArp::busca_todos_routers()
+{
+        QString vector;
+        QString arp;
+        vector=ipsNodosString();
+        arp = arp_scan(ARP_SCAN + vector);
+return arp;
+}
+
+QList<QVariant> GrxArp::consulta_sql(const QString &arg,int columna)
 {
     QSqlQuery consultar;
     QList<QVariant> vector;
     if (consultar.exec(arg)){
        while (consultar.next()){
-          vector.append(consultar.value(i).toString());
+          vector.append(consultar.value(columna).toString());
+       }
+    } else {
+          qDebug()<<"Error No se ha podido realizar la consulta "<< consultar.lastError();
+    }
+
+return vector;
+}
+
+QString GrxArp::consulta_nombre (const QString &ip){
+    QSqlQuery consultar;
+    QString resultado;
+    consultar.prepare(QString("select nombre from NODOS where ip = :ip"));
+    consultar.bindValue(":ip", ip);
+    if (consultar.exec() and consultar.first()){
+            resultado.append(consultar.value(0).toString());
+
+    } else {
+          qDebug()<<"Error No se ha podido realizar la consulta "<< consultar.lastError();
+    }
+return resultado;
+}
+
+QString GrxArp::consulta_ip (const QString &nombre){
+    QSqlQuery consultar;
+    QString resultado;
+    consultar.prepare(QString("select ip from NODOS where nombre = :nombre"));
+    consultar.bindValue(":nombre", nombre);
+    if (consultar.exec() and consultar.first()){
+            resultado.append(consultar.value(0).toString());
+
+    } else {
+          qDebug()<<"Error No se ha podido realizar la consulta "<< consultar.lastError();
+    }
+return resultado;
+}
+
+QString GrxArp::busca_nodo_por_ip(const QString &nodo)
+{
+    QString arp;
+    arp = arp_scan(ARP_SCAN + nodo);
+    QStringList nodoList = arp.split('\n').first().split('\t'); //arp devuelve algo parecido a esto "192.168.1.1\tdd:dd:dd:dd:dd:dd"
+    return nodoList.first();//con esto cogemos solo la ip
+
+
+}
+
+bool GrxArp::esta_nodo_por_ip(const QString &nodo)
+{
+    QString arp;
+    arp = arp_scan(ARP_SCAN + nodo);
+    QStringList nodoList = arp.split('\n').first().split('\t'); //arp devuelve algo parecido a esto "192.168.1.1\tdd:dd:dd:dd:dd:dd"
+
+    if (nodo == nodoList.first()){
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+QString GrxArp::busca_nodo_por_nombre(const QString &nodo)
+{
+    QString arp;
+    QString ip_nodo;
+    ip_nodo = consulta_ip(nodo);
+    qDebug() << ip_nodo;
+    arp = arp_scan(ARP_SCAN + ip_nodo);
+    QStringList nodoList = arp.split('\n').first().split('\t'); //arp devuelve algo parecido a esto "192.168.1.1\tdd:dd:dd:dd:dd:dd"
+    return nodoList.first();//con esto cogemos solo la ip
+
+
+}
+
+bool GrxArp::esta_nodo_por_nombre(const QString &nodo)
+{
+    QString arp;
+    QString ip_nodo;
+    ip_nodo = consulta_ip(nodo);
+    qDebug() << ip_nodo;
+    arp = arp_scan(ARP_SCAN + ip_nodo);
+    QStringList nodoList = arp.split('\n').first().split('\t');
+    if (ip_nodo == nodoList.first()){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+QList<QVariant> GrxArp::consulta_sql_todo(const QString &arg)
+{
+    int col,i;
+    QSqlQuery consultar;
+    QList<QVariant> vector;
+    if (consultar.exec(arg)){
+       QSqlRecord record = consultar.record();
+       col=record.count();
+       while (consultar.next()){
+          for (i=0; i< col;i++){
+                vector.append(consultar.value(i).toString());
+          }
+
        }
     }else{
           qDebug()<<"Error No se ha podido realizar la consulta "<< consultar.lastError();
